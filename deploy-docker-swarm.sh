cf1aa79bd88224ebda3e192afecd640cfa11f009
chris7444 <christophe.lehy@hpe.com>
2016-12-19 18:14:37 +0000

    deploy docker swarm end to end

--- deploy-docker-swarm.sh
+++ deploy-docker-swarm.sh
@@ -1,33 +1,36 @@
-IMAGE="coreos:CoreOS:Stable:latest"
 IMAGE="Canonical:UbuntuServer:16.04-LTS:latest"
 LOCATION="westeurope"
-RG="RGkube"
+RG="docker"
 
-VNET="vnet-kube"
+VNET="vnet-${RG}"
 VNETPREFIX="10.6.0.0/16"
 
+SUBNET="subnet-${RG}"
 CIDR="10.6.0.0/16"
-SUBNET="subnet-kube"
-FIP="fip-kube"
+FIP="fip-${RG}"
+MFQDN=${FIP}.${LOCATION}.cloudapp.azure.com
 
-
-YAML="/tmp/cloud-config.yml"
-CERT="azure.pub"
+NSG="nsg-${RG}"
 
 # we only deploy docker no worker needed
 NB_WORKERS=2
-NSG="nsg-kube"
 
-MASTER="manager"
-MASTER_SIZE="Basic_A1"
+MASTER="manager-${RG}"
+MASTER_SIZE="Basic_A0"
 
-WORKER="worker"
-WORKER_SIZE="Basic_A1"
+WORKER="worker-${RG}"
+WORKER_SIZE="Basic_A0"
+
+ADMIN_USER=core
+YAML="/tmp/cloud-config.yml"
+CERT="azure.pub"
 
 cat <<EOF >$YAML
 #cloud-config
 packages:
   - docker.io
+runcmd:
+  - usermod -a -G docker ${ADMIN_USER}
 EOF
 
 
@@ -50,7 +53,7 @@ EOF
             --custom-data=$YAML \
             --disable-boot-diagnostics \
             --vm-size=$MASTER_SIZE \
-            --admin-username core --ssh-publickey-file $CERT \
+            --admin-username ${ADMIN_USER} --ssh-publickey-file $CERT \
             --nic-name nic-$MASTER  --vnet-name $VNET --vnet-subnet-name $SUBNET \
             --public-ip-name $FIP --public-ip-domain-name $FIP \
             --image-urn $IMAGE \
@@ -67,7 +70,7 @@ EOF
                 --custom-data=$YAML \
                 --disable-boot-diagnostics \
                 --vm-size=$WORKER_SIZE \
-                --admin-username core --ssh-publickey-file $CERT \
+                --admin-username ${ADMIN_USER} --ssh-publickey-file $CERT \
                 --nic-name nic-${WORKER}$i   --vnet-name $VNET --vnet-subnet-name $SUBNET \
                 --image-urn $IMAGE \
                 $RG vm-${WORKER}$i $LOCATION Linux
@@ -81,21 +84,38 @@ EOF
 #azure  network nsg rule create --protocol tcp --destination-port-range 31900-31999 --access allow --direction inbound $RG $NSG  myapps 110
 #azure  network nic set --network-security-group-name $NSG $RG nic-${MASTER}
 
+master_ip=`azure network nic show $RG nic-${MASTER} | awk -F":" '/Private IP address/ { print $3}'`
+
+# create the swarm on the mastr
+ssh-keygen -R ${MFQDN}
+ssh -A ${ADMIN_USER}@${MFQDN}  -o StrictHostKeyChecking=no ifconfig eth0
+ssh -A ${ADMIN_USER}@${MFQDN}  -o StrictHostKeyChecking=no docker swarm init --advertise-addr ${master_ip}
+
+# workers join the swarm
+token=$(ssh -A ${ADMIN_USER}@${MFQDN} docker swarm join-token --quiet worker)
+
+i=1
+while [ $i -le $NB_WORKERS ]
+do
+    priv_ip=`azure network nic show $RG nic-${WORKER}$i | awk -F":" '/Private IP address/ { print $3}'`
+    ssh -A ${ADMIN_USER}@${MFQDN}  -o StrictHostKeyChecking=no ssh  -o StrictHostKeyChecking=no $priv_ip docker swarm join --token $token ${master_ip}:2377
+    ((i++))
+done
+
 #
 # log something
 #
 
-        cluster_fip=$(azure network public-ip list $RG | awk "/$FIP/ {print \$8  \" \" \$10}")
-        echo "Cluster FIP at ${cluster_fip}"
-        priv_ip=`azure network nic show $RG nic-${MASTER} | awk -F":" '/Private IP address/ { print $3}'`
-        echo "Private IP VM ${MASTER} ${priv_ip}"
+cluster_fip=$(azure network public-ip list $RG | awk "/$FIP/ {print \$8  \" \" \$10}")
+echo "Cluster FIP at ${cluster_fip}"
+master_ip=`azure network nic show $RG nic-${MASTER} | awk -F":" '/Private IP address/ { print $3}'`
+echo "Private IP VM ${MASTER} ${master_ip}"
 
-        i=1
-        while [ $i -le $NB_WORKERS ]
-        do
-            priv_ip=`azure network nic show $RG nic-${WORKER}$i | awk -F":" '/Private IP address/ { print $3}'`
-            echo "Private IP VM ${WORKER}${i} ${priv_ip}"
-            ((i++))
-        done
-
+i=1
+while [ $i -le $NB_WORKERS ]
+do
+    priv_ip=`azure network nic show $RG nic-${WORKER}$i | awk -F":" '/Private IP address/ { print $3}'`
+    echo "Private IP VM ${WORKER}${i} ${priv_ip}"
+    ((i++))
+done
 
